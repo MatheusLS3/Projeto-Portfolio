5ee04903b13858e92d794f636a463e98e6ee21de
Matheus Lucena <matheuslucenatt@gmail.com>
2024-03-14 10:51:27 -0300

    primeiro commit

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+/* Início Código da Formação */
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['anterior', 'proximo'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+
+    constructor (container, items, controls) {
+        this.carouselContainer = container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
+    }
+
+    updateGallery(){
+        this.carouselArray.forEach(el => {
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+            el.classList.remove('gallery-item-6');
+        });
+
+        this.carouselArray.slice(0, 5).forEach((el , i) => {
+            el.classList.add(`gallery-item-${i+1}`);
+        });
+    }
+
+    setCurrentState(direction) {
+        if (direction.className == 'gallery-controls-anterior'){
+            this.carouselArray.unshift(this.carouselArray.pop());
+        }else {
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateGallery();
+    }
+
+    setControls() {
+        this.carouselControls.forEach(control => {
+            galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+            document.querySelector(`.gallery-controls-${control}`).innerText = control;
+        });
+    }
+
+    useControls(){
+        const triggers = [...galleryControlsContainer.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control);
+            });
+        });
+    }
+}
+/* Final Código da Formação */
+
+/* Início Código Dark */
+const exempleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exempleCarousel.setControls();
+exempleCarousel.useControls();
+
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    const form = document.getElementById('mai');
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+        form.classList.add('dark');
+        return;
+    }
+
+    mode.classList.add('fa-moon');
+    mode.classList.remove('fa-sun');
+    form.classList.remove('dark');
+});
+/* Final Código Dark */
+
+/* Início Código Menu */
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+/* Final Código Menu */
